7a1be30b704a26d9d59183140c9ffb50935c345e
celestin-001 <127370071+celestin-001@users.noreply.github.com>
2025-05-30 11:41:48 +0200

    last release

--- gestion-finance-WebAPI/GestionAPI/GestionAPI/Controllers/TransactionsController.cs
+++ gestion-finance-WebAPI/GestionAPI/GestionAPI/Controllers/TransactionsController.cs
@@ -1,10 +1,9 @@
-using Business.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Business.Implementations; // Pour .GetFromJsonAsync
 using Microsoft.AspNetCore.Mvc;
-
-
-
+using Business.Interfaces;
 using Common.DTO;
-using Business.Implementations;
 
 namespace GestionAPI.Controllers
 {
@@ -12,19 +11,30 @@ namespace GestionAPI.Controllers
     [Route("api/transactions")]
     public class TransactionsController : Controller
     {
-        private readonly ITransactionService _transactionService;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public TransactionsController(ITransactionService transactionService)
+        public TransactionsController(IHttpClientFactory httpClientFactory)
         {
-            _transactionService = transactionService;
+            _httpClientFactory = httpClientFactory;
         }
 
         [HttpGet("getTransactions")]
-        public IActionResult ExportPdf(DateTime start, DateTime end)
+        public async Task<IActionResult> ExportPdf([FromQuery] DateTime start, [FromQuery] DateTime end)
         {
-            var transactions = _transactionService.GetTransactions(start, end);
+            var bankId = "123";     
+            var accountId = "456";  
+
+            var httpClient = _httpClientFactory.CreateClient("TransactionsClient");
+            var requestUrl = $"/api/opb/transactions/{bankId}/{accountId}?start={start:O}&end={end:O}";
+
+            var transactions = await httpClient.GetFromJsonAsync<List<TransactionDto>>(requestUrl);
+
+            if (transactions == null)
+                return NotFound("Aucune transaction trouvée.");
+
             var pdfBytes = PdfExporter.ExportTransactions(transactions, start, end);
             var fileName = $"transactions_{start:yyyyMMdd}_{end:yyyyMMdd}.pdf";
+
             return File(pdfBytes, "application/pdf", fileName);
         }
     }
